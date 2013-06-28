05b2763d535950a1610c59ea6e8c91e99cfbc286
John Stovin <john.d.stovin@gmail.co.uk>
2013-06-28 08:16:50 +0100

    Searchy thing

---
+++ TwitterSearch/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reactive.Linq;
+
+using ReactiveUI;
+
+namespace TwitterSearch
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        public MainWindowViewModel()
+        {
+            Changed.Throttle(new TimeSpan(0, 0, 1)).Subscribe(
+                x =>
+                {
+                    if (x.PropertyName == "SearchText")
+                        SearchFor = SearchText;
+                });
+        }
+
+
+        private string searchText;
+        public string SearchText
+        {
+            get { return searchText; }
+            set { this.RaiseAndSetIfChanged(ref searchText, value); }
+        }
+
+        private string searchFor;
+        public string SearchFor
+        {
+            get { return searchFor; }
+            set { this.RaiseAndSetIfChanged(ref searchFor, value); }
+        }
+    }
+}
